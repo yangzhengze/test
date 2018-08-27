6ba629612004a9fc0475dbc721e9930132487964
yangzhengze <1403014932@qq.com>
2018-08-27 19:54:46 +0800

    test

---
+++ src/com/zpark/chapter02/Hello.java
@@ -0,0 +1,19 @@
+package com.zpark.chapter02;
+
+public class Hello {
+	//main方法，所有java程序的入口（唯一一个必要的方法）
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+		//变量的声明,方式1：
+		int a;//声明了一个名字为a的整形变量
+		a = 1000;//赋值
+		
+		//变量声明方式2：
+		int b = 1000;
+		
+		//变量的使用
+		System.out.println(a);
+		
+	}
+}
